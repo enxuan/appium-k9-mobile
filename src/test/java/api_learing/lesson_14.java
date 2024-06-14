f951dc670c6a21f1586f15ce297004109f93a3bc
enxuan <dtex89@gmail.com>
2024-06-14 11:42:17 +0700

    add code for lesson_14

---
+++ src/test/java/api_learing/lesson_14.java
@@ -0,0 +1,40 @@
+package api_learing;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+
+public class lesson_14 {
+
+    public static void main(String[] args) {
+        //send a request into Appium Server < ask to launch the app
+
+        AppiumDriver<MobileElement> appiumDriver = null;
+
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("platformName", "Android");
+        desiredCapabilities.setCapability("automationName", "uiautomator2");
+        desiredCapabilities.setCapability("udid", "RF8W40F0GKF");
+        desiredCapabilities.setCapability("appPackage", "com.wdiodemoapp");
+        desiredCapabilities.setCapability("appActivity", "com.wdiodemoapp.MainActivity");
+
+        try {//Init appium session
+        URL appiumServer = new URL("http://localhost:4723/wd/hub");
+        appiumDriver = new AndroidDriver<>(appiumServer, desiredCapabilities);
+
+        //DEBUG PURPOSE ONLY
+        Thread.sleep(10000);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (appiumDriver != null) {
+            appiumDriver.quit();
+        }
+
+
+    }
+}
